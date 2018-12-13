150166cc1d48e23d85f0eaa06a330393bb07fb98
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-12-13 10:31:57 -0700

    Create CirclesSquaresTest.java

---
+++ CirclesSquaresTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class CirclesSquaresTest
+{
+	public static void main(String[ ] args)
+	{
+		JFrame frame = new MyFrame( );
+		frame.setTitle("Circles and Squares");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
